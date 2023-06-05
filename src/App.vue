0a4fa6584dd8818d477611cabf573962cc008534
yc <arigatouyc@foxmail.com>
2023-06-05 23:52:20 +0800

    1.0

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import SvgIcon from "./components/SvgIcon/index.vue";
+</script>
+
+<template>
+    <el-button type="primary">Hello</el-button>
+        <SvgIcon name="phone" color="red" width="200px" height="200px"></SvgIcon>
+</template>
+
+<style scoped></style>
---
+++ src/components/SvgIcon/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <svg :style="{ width, height }">
+        <use :xlink:href="prefix + name" :fill="color" :width="width" :height="height"></use>
+    </svg>
+</template>
+
+<script setup lang="ts">
+defineProps({
+    prefix: {
+        type: String,
+        default: '#icon-'
+    },
+    name: {
+        type: String
+    },
+    color: {
+        type: String
+    },
+    width: {
+        type: String
+    },
+    height: {
+        type: String
+    }
+})
+</script>
+
+<style scoped></style>
